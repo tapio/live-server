d096b6147b398879d19ed7178e65a40090c76f65
Irakli Nadareishvili <irakli@gmail.com>
2015-01-08 02:42:16 -0500

    Update index.js

--- index.js
+++ index.js
@@ -74,7 +74,7 @@ LiveServer.start = function(port, directory, suppressBrowserLaunch) {
 		.use(staticServer(directory)) // Custom static server
 		.use(connect.directory(directory, { icons: true }))
 		.use(connect.logger('dev'));
-	var server = http.createServer(app).listen(port);
+	var server = http.createServer(app).listen(port, '0.0.0.0');
 	// WebSocket
 	server.addListener('upgrade', function(request, socket, head) {
 		ws = new WebSocket(request, socket, head);
@@ -104,12 +104,11 @@ LiveServer.start = function(port, directory, suppressBrowserLaunch) {
 		}
 	});
 	// Output
-	console.log(('Serving "' + directory + '" at http://localhost:' + port).green);
+	console.log(('Serving "' + directory + '" at http://0.0.0.0:' + port).green);
 
 	// Launch browser
 	if(!suppressBrowserLaunch)
-		open('http://localhost:' + port);
+		open('http://0.0.0.0:' + port);
 }
 
 module.exports = LiveServer;
-
