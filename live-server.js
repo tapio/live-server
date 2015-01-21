a3920912e4f6e42edaccd71fbc3d79cf6bfde0dc
Pomax <pomax@nihongoresources.com>
2015-01-21 11:21:04 -0800

    allow port specification from cli

--- live-server.js
+++ live-server.js
@@ -1,7 +1,20 @@
 #!/usr/bin/env node
 var liveServer = require("./index");
 
+var port = process.env.PORT;
+
+process.argv.forEach(function(v,idx) {
+	if (v.indexOf("--port=") >- 1) {
+		var portString = v.substring(7); 
+        var portNumber = parseInt(portString, 10);
+		if (portNumber == portString) {
+			port = portNumber;
+    		process.argv.splice(idx,1);
+    	}
+	}
+});
+
 if (process.argv[2])
 	process.chdir(process.argv[2]);
 
-liveServer.start(process.env.PORT);
+liveServer.start(port);
