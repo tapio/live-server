c6cb6e621faf29c0c45ea90b35e96a947c248b0e
Tapio Vierros <tapiovierros@gmail.com>
2015-12-06 14:47:59 +0200

    Some very basic cli tests.

--- test/cli.js
+++ test/cli.js
@@ -1,5 +1,25 @@
-describe('command line usage: ', function(){
-	xit('correctly reads arguments', function(done){
-		done(); // todo
+var assert = require('assert');
+var path = require('path');
+var exec = require('child_process').execFile;
+var cmd = path.join(__dirname, "..", "live-server.js");
+var opts = {
+	timeout: 1000,
+	maxBuffer: 1024
+};
+
+describe('command line usage', function() {
+	it('--version', function(done) {
+		exec(cmd, [ "--version" ], opts, function(error, stdout, stdin) {
+			assert(!error, error);
+			assert(stdout.indexOf("live-server") == 0);
+			done();
+		});
+	});
+	it('--help', function(done) {
+		exec(cmd, [ "--help" ], opts, function(error, stdout, stdin) {
+			assert(!error, error);
+			assert(stdout.indexOf("Usage: live-server") == 0);
+			done();
+		});
 	});
 });
