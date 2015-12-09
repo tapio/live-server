9fd1e8445300d83796855fb6bd9ec1df9df5c34e
Tapio Vierros <tapiovierros@gmail.com>
2015-12-09 18:07:24 +0200

    Test for --quiet.

--- test/cli.js
+++ test/cli.js
@@ -3,7 +3,7 @@ var path = require('path');
 var exec = require('child_process').execFile;
 var cmd = path.join(__dirname, "..", "live-server.js");
 var opts = {
-	timeout: 1000,
+	timeout: 2000,
 	maxBuffer: 1024
 };
 
@@ -11,14 +11,21 @@ describe('command line usage', function() {
 	it('--version', function(done) {
 		exec(cmd, [ "--version" ], opts, function(error, stdout, stdin) {
 			assert(!error, error);
-			assert(stdout.indexOf("live-server") == 0);
+			assert(stdout.indexOf("live-server") == 0, "version not found");
 			done();
 		});
 	});
 	it('--help', function(done) {
 		exec(cmd, [ "--help" ], opts, function(error, stdout, stdin) {
 			assert(!error, error);
-			assert(stdout.indexOf("Usage: live-server") == 0);
+			assert(stdout.indexOf("Usage: live-server") == 0, "usage not found");
+			done();
+		});
+	});
+	it('--quiet', function(done) {
+		exec(cmd, [ "--quiet", "--no-browser", "--test" ], opts, function(error, stdout, stdin) {
+			assert(!error, error);
+			assert(stdout === "", "stdout not empty");
 			done();
 		});
 	});
