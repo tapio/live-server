b3721465ef2597d5c74654ff10939f4e5f668e63
Tapio Vierros <tapiovierros@gmail.com>
2016-02-18 20:02:30 +0200

    Tyop fixes and one more test for htpasswd.

--- test/htpasswd.js
+++ test/htpasswd.js
@@ -4,22 +4,25 @@ var liveServer = require('..').start({
 	root: path.join(__dirname, "data"),
 	port: 0,
 	open: false,
-	mount: [[ "/mounted", path.join(__dirname, "data", "sub") ]],
 	htpasswd: path.join(__dirname, "data", "htpasswd-test")
 });
 
-describe('htpasswd tets', function() {
-	it('should respond with 401 since to password is set', function(done) {
+describe('htpasswd tests', function() {
+	it('should respond with 401 since no password is given', function(done) {
 		request(liveServer)
 			.get('/')
 			.expect(401, done);
 	});
-
-	it('should respond with 200 since correct password is set', function(done) {
+	it('should respond with 401 since wrong password is given', function(done) {
+		request(liveServer)
+			.get('/')
+			.auth("test", "not-real-password")
+			.expect(401, done);
+	});
+	it('should respond with 200 since correct password is given', function(done) {
 		request(liveServer)
 			.get('/')
 			.auth("test", "test")
 			.expect(200, done);
 	});
-
 });
