1e07ddf2dccef8846af1f2b75bfcb9fe0db9ce1d
pavel <pavel.github@outlook.com>
2016-04-23 01:54:53 -0400

    Adds a test for file mounting support

--- test/mount.js
+++ test/mount.js
@@ -4,7 +4,10 @@ var liveServer = require('..').start({
 	root: path.join(__dirname, "data"),
 	port: 0,
 	open: false,
-	mount: [[ "/mounted", path.join(__dirname, "data", "sub") ]]
+	mount: [
+		[ "/mounted", path.join(__dirname, "data", "sub") ],
+		[ "/style", path.join(__dirname, "data", "style.css") ]
+	]
 });
 
 describe('mount tests', function() {
@@ -13,6 +16,13 @@ describe('mount tests', function() {
 			.get('/mounted/sub.html')
 			.expect('Content-Type', 'text/html; charset=UTF-8')
 			.expect(/Subdirectory/i)
+			.expect(200, done);
+	});
+	it('should respond with style.css', function(done) {
+		request(liveServer)
+			.get('/style')
+			.expect('Content-Type', 'text/css; charset=UTF-8')
+			.expect(/color/i)
 			.expect(200, done);
 	});
 });
