6a8c717aafdc234f2ef24a888c92810c8ecc2e6c
Tapio Vierros <tapiovierros@gmail.com>
2018-11-26 22:18:24 +0200

    Test fixes (courtesy of @ajbogh).

--- test/cli.js
+++ test/cli.js
@@ -17,14 +17,14 @@ describe('command line usage', function() {
 	it('--version', function(done) {
 		exec_test([ "--version" ], function(error, stdout, stdin) {
 			assert(!error, error);
-			assert(stdout.indexOf("live-server") == 0, "version not found");
+			assert(stdout.indexOf("live-server") === 0, "version not found");
 			done();
 		});
 	});
 	it('--help', function(done) {
 		exec_test([ "--help" ], function(error, stdout, stdin) {
 			assert(!error, error);
-			assert(stdout.indexOf("Usage: live-server") == 0, "usage not found");
+			assert(stdout.indexOf("Usage: live-server") === 0, "usage not found");
 			done();
 		});
 	});
@@ -38,7 +38,7 @@ describe('command line usage', function() {
 	it('--port', function(done) {
 		exec_test([ "--port=16123", "--no-browser", "--test" ], function(error, stdout, stdin) {
 			assert(!error, error);
-			assert(stdout.indexOf("Serving") == 0, "serving string not found");
+			assert(stdout.indexOf("Serving") >= 0, "serving string not found");
 			assert(stdout.indexOf("at http://127.0.0.1:16123") != -1, "port string not found");
 			done();
 		});
@@ -46,7 +46,7 @@ describe('command line usage', function() {
 	it('--host', function(done) {
 		exec_test([ "--host=localhost", "--no-browser", "--test" ], function(error, stdout, stdin) {
 			assert(!error, error);
-			assert(stdout.indexOf("Serving") == 0, "serving string not found");
+			assert(stdout.indexOf("Serving") >= 0, "serving string not found");
 			assert(stdout.indexOf("at http://localhost:") != -1, "host string not found");
 			done();
 		});
@@ -58,7 +58,7 @@ describe('command line usage', function() {
 				"--test"
 			], function(error, stdout, stdin) {
 			assert(!error, error);
-			assert(stdout.indexOf("Serving") == 0, "serving string not found");
+			assert(stdout.indexOf("Serving") >= 0, "serving string not found");
 			done();
 		});
 	});
